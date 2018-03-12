c74233bdd27081e6f986a940ab159dee339606e8
王明 <3081515830@qq.com>
2018-03-12 19:15:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ PORT = '3306'
 DATABASE = 'like_zhihu'
 USERNAME = 'root'
 # 这里的数据库密码改成自己的
-PASSWORD = 'ltyq'
+PASSWORD = 'xxxx'
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 # 关闭跟踪修改
 SQLALCHEMY_TRACK_MODIFICATIONS = False
